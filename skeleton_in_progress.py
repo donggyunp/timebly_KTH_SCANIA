b1b538f73590d2cc88fc701819cb51e40390f8d6
Donggyun Park <donggyun@kth.se>
2023-05-02 17:02:33 +0200

    Extracting skeleton coordinates, ignoring the frames with Nonetype

--- skeleton_in_progress.py
+++ skeleton_in_progress.py
@@ -12,8 +12,6 @@ import pandas as pd
 import mediapipe as mp
 import numpy as np
 
-from skeleton_to_csv import extract_skeleton
-
 csv_config_path = sys.argv[1]
 video_path = sys.argv[2]
 csv_coords_dir = sys.argv[3]
@@ -39,9 +37,13 @@ print(frames, fps, seconds)
 #t.start()
 
 for start, end, label in zip(start_time, end_time, labels):
-    idx = round(start,1)*fps
+    idx = round(start,1)*round(fps,0)
     #idx = -1
-    LS, RS, LE, RE, LW, RW, LP, RP, LI, RI, LT, RT, LH, RH, LK, RK, LHE, RHE, LF, RF\
+    LS_x, RS_x, LE_x, RE_x, LW_x, RW_x, LP_x, RP_x, LI_x, RI_x, LT_x, RT_x, LH_x, RH_x, LK_x, RK_x, LHE_x, RHE_x, LF_x, RF_x\
+    = [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
+    LS_y, RS_y, LE_y, RE_y, LW_y, RW_y, LP_y, RP_y, LI_y, RI_y, LT_y, RT_y, LH_y, RH_y, LK_y, RK_y, LHE_y, RHE_y, LF_y, RF_y\
+    = [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
+    LS_z, RS_z, LE_z, RE_z, LW_z, RW_z, LP_z, RP_z, LI_z, RI_z, LT_z, RT_z, LH_z, RH_z, LK_z, RK_z, LHE_z, RHE_z, LF_z, RF_z\
     = [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
     label_list = []
     with mp_pose.Pose(
@@ -82,30 +84,53 @@ for start, end, label in zip(start_time, end_time, labels):
             keypoints = pose.process(image)
             lm = keypoints.pose_landmarks
             lmPose  = mp_pose.PoseLandmark
+            if type(lm) == type(None):
+                print("No pose detected")
+                continue
             label_list.append(label)
-            LS.append(lm.landmark[lmPose.LEFT_SHOULDER].x), RS.append(lm.landmark[lmPose.RIGHT_SHOULDER].x)
-            LE.append(lm.landmark[lmPose.LEFT_ELBOW].x), RE.append(lm.landmark[lmPose.RIGHT_ELBOW].x)
-            LW.append(lm.landmark[lmPose.LEFT_WRIST].x), RW.append(lm.landmark[lmPose.RIGHT_WRIST].x)
-            LP.append(lm.landmark[lmPose.LEFT_PINKY].x), RP.append(lm.landmark[lmPose.RIGHT_PINKY].x)
-            LI.append(lm.landmark[lmPose.LEFT_INDEX].x), RI.append(lm.landmark[lmPose.RIGHT_INDEX].x)
-            LT.append(lm.landmark[lmPose.LEFT_THUMB].x), RT.append(lm.landmark[lmPose.RIGHT_THUMB].x)
-            LH.append(lm.landmark[lmPose.LEFT_HIP].x), RH.append(lm.landmark[lmPose.RIGHT_HIP].x)
-            LK.append(lm.landmark[lmPose.LEFT_KNEE].x), RK.append(lm.landmark[lmPose.RIGHT_KNEE].x)
-            LHE.append(lm.landmark[lmPose.LEFT_HEEL].x), RHE.append(lm.landmark[lmPose.RIGHT_HEEL].x)
-            LF.append(lm.landmark[lmPose.LEFT_FOOT_INDEX].x), RF.append(lm.landmark[lmPose.RIGHT_FOOT_INDEX].x)
+            LS_x.append(lm.landmark[lmPose.LEFT_SHOULDER].x), RS_x.append(lm.landmark[lmPose.RIGHT_SHOULDER].x)
+            LS_y.append(lm.landmark[lmPose.LEFT_SHOULDER].y), RS_y.append(lm.landmark[lmPose.RIGHT_SHOULDER].y)
+            LS_z.append(lm.landmark[lmPose.LEFT_SHOULDER].z), RS_z.append(lm.landmark[lmPose.RIGHT_SHOULDER].z)
+            LE_x.append(lm.landmark[lmPose.LEFT_ELBOW].x), RE_x.append(lm.landmark[lmPose.RIGHT_ELBOW].x)
+            LE_y.append(lm.landmark[lmPose.LEFT_ELBOW].y), RE_y.append(lm.landmark[lmPose.RIGHT_ELBOW].y)
+            LE_z.append(lm.landmark[lmPose.LEFT_ELBOW].z), RE_z.append(lm.landmark[lmPose.RIGHT_ELBOW].z)
+            LW_x.append(lm.landmark[lmPose.LEFT_WRIST].x), RW_x.append(lm.landmark[lmPose.RIGHT_WRIST].x)
+            LW_y.append(lm.landmark[lmPose.LEFT_WRIST].y), RW_y.append(lm.landmark[lmPose.RIGHT_WRIST].y)
+            LW_z.append(lm.landmark[lmPose.LEFT_WRIST].z), RW_z.append(lm.landmark[lmPose.RIGHT_WRIST].z)
+            LP_x.append(lm.landmark[lmPose.LEFT_PINKY].x), RP_x.append(lm.landmark[lmPose.RIGHT_PINKY].x)
+            LP_y.append(lm.landmark[lmPose.LEFT_PINKY].y), RP_y.append(lm.landmark[lmPose.RIGHT_PINKY].y)
+            LP_z.append(lm.landmark[lmPose.LEFT_PINKY].z), RP_z.append(lm.landmark[lmPose.RIGHT_PINKY].z)
+            LI_x.append(lm.landmark[lmPose.LEFT_INDEX].x), RI_x.append(lm.landmark[lmPose.RIGHT_INDEX].x)
+            LI_y.append(lm.landmark[lmPose.LEFT_INDEX].y), RI_y.append(lm.landmark[lmPose.RIGHT_INDEX].y)
+            LI_z.append(lm.landmark[lmPose.LEFT_INDEX].z), RI_z.append(lm.landmark[lmPose.RIGHT_INDEX].z)
+            LT_x.append(lm.landmark[lmPose.LEFT_THUMB].x), RT_x.append(lm.landmark[lmPose.RIGHT_THUMB].x)
+            LT_y.append(lm.landmark[lmPose.LEFT_THUMB].y), RT_y.append(lm.landmark[lmPose.RIGHT_THUMB].y)
+            LT_z.append(lm.landmark[lmPose.LEFT_THUMB].z), RT_z.append(lm.landmark[lmPose.RIGHT_THUMB].z)
+            LH_x.append(lm.landmark[lmPose.LEFT_HIP].x), RH_x.append(lm.landmark[lmPose.RIGHT_HIP].x)
+            LH_y.append(lm.landmark[lmPose.LEFT_HIP].y), RH_y.append(lm.landmark[lmPose.RIGHT_HIP].y)
+            LH_z.append(lm.landmark[lmPose.LEFT_HIP].z), RH_z.append(lm.landmark[lmPose.RIGHT_HIP].z)
+            LK_x.append(lm.landmark[lmPose.LEFT_KNEE].x), RK_x.append(lm.landmark[lmPose.RIGHT_KNEE].x)
+            LK_y.append(lm.landmark[lmPose.LEFT_KNEE].y), RK_y.append(lm.landmark[lmPose.RIGHT_KNEE].y)
+            LK_z.append(lm.landmark[lmPose.LEFT_KNEE].z), RK_z.append(lm.landmark[lmPose.RIGHT_KNEE].z)
+            LHE_x.append(lm.landmark[lmPose.LEFT_HEEL].x), RHE_x.append(lm.landmark[lmPose.RIGHT_HEEL].x)
+            LHE_y.append(lm.landmark[lmPose.LEFT_HEEL].y), RHE_y.append(lm.landmark[lmPose.RIGHT_HEEL].y)
+            LHE_z.append(lm.landmark[lmPose.LEFT_HEEL].z), RHE_z.append(lm.landmark[lmPose.RIGHT_HEEL].z)
+            LF_x.append(lm.landmark[lmPose.LEFT_FOOT_INDEX].x), RF_x.append(lm.landmark[lmPose.RIGHT_FOOT_INDEX].x)
+            LF_y.append(lm.landmark[lmPose.LEFT_FOOT_INDEX].y), RF_y.append(lm.landmark[lmPose.RIGHT_FOOT_INDEX].y)
+            LF_z.append(lm.landmark[lmPose.LEFT_FOOT_INDEX].z), RF_z.append(lm.landmark[lmPose.RIGHT_FOOT_INDEX].z)
             if idx >= round(end,1)*fps:
                 print("extracting one action ended")
                 coords_df = pd.DataFrame(
-                    {'LS': LS,'RS': RS,#shoulder
-                        'LE': LE,'RE': RE,#elbow
-                        'LW': LW,'RW': RW,#wrist
-                        'LP': LP,'RP': RP,#pinkie
-                        'LI': LI,'RI': RI,#index
-                        'LT': LT,'RT': RT,#thumb
-                        'LH': LH,'RH': RH,#hip
-                        'LK': LF,'RK': RK,#knee
-                        'LHE': LHE,'RHE': RHE,#heel
-                        'LF': LF,'RF': RF,#foot
+                    {   'LS_x': LS_x,'RS_x': RS_x,'LS_y': LS_y,'RS_y': RS_y,'LS_z': LS_z,'RS_z': RS_z,              #shoulder
+                        'LE_x': LE_x,'RE_x': RE_x,'LE_y': LE_y,'RE_y': RE_y,'LE_z': LE_z,'RE_z': RE_z,              #elbow
+                        'LW_x': LW_x,'RW_x': RW_x,'LW_y': LW_y,'RW_y': RW_y,'LW_z': LW_z,'RW_z': RW_z,              #wrist
+                        'LP_x': LP_x,'RP_x': RP_x,'LP_y': LP_y,'RP_y': RP_y,'LP_z': LP_z,'RP_z': RP_z,              #pinkie
+                        'LI_x': LI_x,'RI_x': RI_x,'LI_y': LI_y,'RI_y': RI_y,'LI_z': LI_z,'RI_z': RI_z,              #index
+                        'LT_x': LT_x,'RT_x': RT_x,'LT_y': LT_y,'RT_y': RT_y,'LT_z': LT_z,'RT_z': RT_z,              #thumb
+                        'LH_x': LH_x,'RH_x': RH_x,'LH_y': LH_y,'RH_y': RH_y,'LH_z': LH_z,'RH_z': RH_z,              #hip
+                        'LK_x': LF_x,'RK_x': RK_x,'LK_y': LF_y,'RK_y': RK_y,'LK_z': LF_z,'RK_z': RK_z,              #knee
+                        'LHE_x': LHE_x,'RHE_x': RHE_x,'LHE_y': LHE_y,'RHE_y': RHE_y,'LHE_z': LHE_z,'RHE_z': RHE_z,  #heel
+                        'LF_x': LF_x,'RF_x': RF_x,'LF_y': LF_y,'RF_y': RF_y,'LF_z': LF_z,'RF_z': RF_z,              #foot
                         'label': label_list
                     }
                 )
