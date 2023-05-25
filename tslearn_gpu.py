eacb8386d4e3abcc1529ebcef2b8e11e54855a01
Donggyun Park <donggyun@kth.se>
2023-05-25 12:18:47 +0200

    in progress

--- tslearn_gpu.py
+++ tslearn_gpu.py
@@ -17,19 +17,24 @@ from tslearn.preprocessing import TimeSeriesScalerMinMax
 from tslearn.utils import to_time_series_dataset
 from tslearn.svm import TimeSeriesSVC
 
+import dpctl
+from sklearnex import patch_sklearn, config_context
+patch_sklearn()
+
+C_grid = [0.8, 0.9, 1.0, 1.1, 1.2,]
+gamma_grid = [0.05, 0.075, 0.1, 0.125, 0.15]
 
 @jit(target_backend='cuda') 
-def tslearn_gpu(feature, label):
-    C_grid = [0.8, 0.9, 1.0, 1.1, 1.2,]
-    gamma_grid = [0.05, 0.075, 0.1, 0.125, 0.15]
-    for i in C_grid:
-        for j in gamma_grid:
-            print(i, j)
-            clf = TimeSeriesSVC(C = i, kernel = 'gak', gamma = j)
-            # the other kernels are not available for different lengths of time series.
-            clf.fit(ts_dataset_X, ts_dataset_y)
-            scores = cross_val_score(clf, ts_dataset_X, ts_dataset_y, cv=5)
-            print(scores, sum(scores)/5)
+def tslearn_gpu(feature, label, c, gamma):
+    print(c, gamma)
+    clf = TimeSeriesSVC(C = i, kernel = 'gak', gamma = j)
+    # the other kernels are not available for different lengths of time series.
+    clf.fit(ts_dataset_X, ts_dataset_y)
+    scores = cross_val_score(clf, ts_dataset_X, ts_dataset_y, cv=5)
+    print(scores, sum(scores)/5)
+
+def tslearn_opt(feature, label):
+    pass
 
 csv_path = 'csv_coords_files'
 csv_files = glob.glob(csv_path + "/*.csv")
@@ -43,5 +48,10 @@ for file in csv_files:
 
 ts_dataset_X = to_time_series_dataset(ts_dataset_X)
 ts_Dataset_X = TimeSeriesScalerMinMax().fit_transform(ts_dataset_X)
-
-tslearn_gpu(ts_dataset_X, ts_dataset_y)
+for i in C_grid:
+    for j in gamma_grid:
+        print('tslrean_gpu starts')
+        tslearn_gpu(ts_dataset_X, ts_dataset_y, i, j)
+        print('tslearn_sklearnex starts')
+        with config_context(target_offload="gpu:0"):
+            clf = TimeSeriesSVC(C = i, kernel = 'gak', gamma = j).fit(ts_dataset_X, ts_dataset_y)
