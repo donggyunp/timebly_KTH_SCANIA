884caa4bfc49984b27ccc8d8905edd7a2cabc617
Donggyun Park <donggyun@kth.se>
2023-05-25 18:21:48 +0200

    in progress

--- tslearn_gpu.py
+++ tslearn_gpu.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 #from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
+#from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
 from tslearn.preprocessing import TimeSeriesScalerMinMax
 from tslearn.utils import to_time_series_dataset
